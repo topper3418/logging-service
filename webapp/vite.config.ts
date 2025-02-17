afc33b294905978b5cf271c578cfa02d6eea31ca
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-02-17 11:08:06 -0600

    attempted to fix the favicon issue

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,10 +9,24 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  base: "./",
   server: {
     proxy: {
       '/logs': 'http://localhost:8080',
       '/loggers': 'http://localhost:8080'
+    },
+    fs: {
+      allow: [
+        path.resolve(__dirname, 'public'), // Allow public assets
+        path.resolve(__dirname, 'src'), // Allow source files
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      }
     }
   }
 })
